8fea62d9e513e06d38c04f62718e59e4a2454f8d
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-05-26 21:32:51 -0300

    feat: create model for post

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,21 @@
-export default function HomePage() {
-  return (<>Hello world!!</>)
+import clsx from "clsx";
+
+export default async function HomePage() {
+  return (
+    <div>
+      <h1
+        className={clsx(
+          "text-xl",
+          "font-bold",
+          "text-blue-500",
+          "hover:text-blue-50",
+          "hover:bg-blue-500",
+          "transition",
+          "duration-300"
+        )}
+      >
+        Olá
+      </h1>
+    </div>
+  );
 }
---
+++ src/models/post/post-model.ts
@@ -0,0 +1,12 @@
+export type PostModel = {
+  id: string,
+  title: string,
+  slug: string,
+  excerpt: string,
+  content: string,
+  converImageUrl: string,
+  published: boolean,
+  createAt: string,
+  updateAt: string,
+  author: string,
+}
