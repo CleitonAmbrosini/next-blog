3b3dac5d4efbdd9c4f679ff9474111102117cca4
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-06-03 23:20:07 -0300

    feat: create breakpoints for Header

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,19 @@
-import { ThemeToggle } from "../ToggleTheme";
+import clsx from 'clsx';
+import { ThemeToggle } from '../ToggleTheme';
 
 export function Header() {
   return (
     <header className='flex align-items-center justify-center py-8 relative'>
-      <h1 className='text-6xl font-bold text-center'>Aqui é a header</h1>
+      <h1
+        className={clsx(
+          'text-4xl/normal font-extrabold',
+          'sm:text-5xl/normal',
+          'md:text-6xl/normal',
+          'lg:text-7xl/normal'
+        )}
+      >
+        <a href='#'>The blog</a>
+      </h1>
       <ThemeToggle />
     </header>
   );
