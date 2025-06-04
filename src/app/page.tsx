8e732e3a2e86e107e82ec79213dd1c3a9c95eade
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-06-03 22:57:21 -0300

    feat: create Container component and adjust toogle position

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+import { Container } from '@/components/Container';
 import { PostList } from '@/components/PostList/inde';
 import { SpinLoader } from '@/components/SpinLoader';
 import ThemeToggle from '@/components/ToggleTheme/ToggleTheme';
@@ -5,9 +6,9 @@ import { Suspense } from 'react';
 
 export default async function HomePage() {
   return (
-    <div className='text-slate-900 bg-slate-100 min-h-screen dark:bg-slate-900 dark:text-slate-100'>
-      <header>
-        <h1 className='text-6xl font-bold text-center py-8'>Aqui é a header</h1>
+    <Container>
+      <header className='flex align-items-center justify-center py-8 relative'>
+        <h1 className='text-6xl font-bold text-center'>Aqui é a header</h1>
         <ThemeToggle />
       </header>
       <Suspense fallback={<SpinLoader />}>
@@ -16,6 +17,6 @@ export default async function HomePage() {
       <footer>
         <p className='text-6xl font-bold text-center py-8'>Aqui é o footer</p>
       </footer>
-    </div>
+    </Container>
   );
 }
--- src/components/ToggleTheme/ToggleTheme.tsx
+++ src/components/ToggleTheme/ToggleTheme.tsx
@@ -1,6 +1,7 @@
 'use client';
 
 import { useTheme } from '@/store/ThemeContext/ThemeContext';
+import clsx from 'clsx';
 import { Moon, Sun } from 'lucide-react';
 
 export default function ThemeToggle() {
@@ -9,11 +10,30 @@ export default function ThemeToggle() {
   return (
     <button
       onClick={toggleTheme}
-      className='p-2 rounded-full border border-gray-300 dark:border-gray-600
-                 bg-white dark:bg-gray-800 text-black dark:text-white hover:scale-105 transition'
+      className={clsx(
+        'rounded-full',
+        'border',
+        'border-gray-300',
+        'dark:border-gray-600',
+        'bg-white',
+        'dark:bg-gray-800',
+        'text-black',
+        'dark:text-white',
+        'hover:scale-105',
+        'transition',
+        'w-7',
+        'h-7',
+        'cursor-pointer',
+        'absolute',
+        'right-0',
+      )}
       aria-label='Alternar tema'
     >
-      {theme === 'dark' ? <Sun size={20} /> : <Moon size={20} />}
+      {theme === 'dark' ? (
+        <Sun size={22} className='m-auto' />
+      ) : (
+        <Moon size={22} className='m-auto' />
+      )}
     </button>
   );
 }
