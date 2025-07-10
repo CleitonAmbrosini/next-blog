afea7061834151da0a4b07f7615ac854294e8cbd
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-07-09 23:42:11 -0300

    feat: create metadata for dynamic post page

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -2,18 +2,21 @@ import clsx from 'clsx';
 
 export default function NotFoundPage() {
   return (
-    <div
-      className={clsx(
-        'min-h-[320px] bg-slate-900 text-slate-100',
-        'mb-16 p-8 rounded-xl',
-        'flex items-center justify-center',
-        'text-center',
-      )}
-    >
-      <div>
-        <h1 className='text-7xl/tight mb-8'>404</h1>
-        <p>Erro 404 - A página que você está tentando acessar não existe.</p>
+    <>
+      <title>Page not found!</title>
+      <div
+        className={clsx(
+          'min-h-[320px] bg-slate-900 text-slate-100',
+          'mb-16 p-8 rounded-xl',
+          'flex items-center justify-center',
+          'text-center',
+        )}
+      >
+        <div>
+          <h1 className='text-7xl/tight mb-8'>404</h1>
+          <p>Erro 404 - The page you are trying to access does not exist.</p>
+        </div>
       </div>
-    </div>
+    </>
   );
 }
