57b0331af4e78c362e72ab35f3cf7f673cb3a9fa
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-08-13 23:44:47 -0300

    style: style dialog component

--- src/components/PostsListAdmin/PostsListAdmin.tsx
+++ src/components/PostsListAdmin/PostsListAdmin.tsx
@@ -30,6 +30,46 @@ export default async function PostsListAdmin() {
           </div>
         );
       })}
+
+      <div
+        className={clsx(
+          'fixed inset-0 z-50',
+          'bg-black/50 backdrop-blur-xs',
+          'flex items-center justify-center',
+        )}
+      >
+        <div
+          className={clsx(
+            'bg-slate-100 p-6 rounded-lg max-w-2xl mx-6',
+            'flex flex-col gap-6',
+            'shadow-lg shadow-black/30',
+          )}
+        >
+          <h3 className='tex-xl font-bold'>Title</h3>
+          <p>content</p>
+          <div className='flex items-center justify-around gap-4'>
+            <button
+              className={clsx(
+                'bg-slate-200 hover:bg-slate-300 text-slate-950 transition',
+                'flex items-center justify-center',
+                'py-2 px-6 rounded-lg cursor-pointer',
+              )}
+              autoFocus
+            >
+              Cancel
+            </button>
+            <button
+              className={clsx(
+                'bg-blue-500 hover:bg-blue-600 text-slate-200 transition',
+                'flex items-center justify-center',
+                'py-2 px-6 rounded-lg cursor-pointer',
+              )}
+            >
+              Continue
+            </button>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
