949a01b1e8d7f1ec66c500efdfb044ca2790306b
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-08-28 23:28:35 -0300

    feat: create Button component

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,5 +1,11 @@
+import { Button } from '@/components/Button';
+
 export const dynamic = 'force-static';
 
 export default async function AdminPostNewPage() {
-  return <div className='py-16 text-6xl'>AdminPostNewPage </div>;
+  return (
+    <div className='py-16 text-6xl'>
+      <Button> Create new </Button>
+    </div>
+  );
 }
