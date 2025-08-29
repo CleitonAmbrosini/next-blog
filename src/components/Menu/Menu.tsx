73e3a2cb93768fe2fa27673975ab4d98db778ce8
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-08-28 22:56:33 -0300

    style: style navigation menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,13 +1,34 @@
+import clsx from 'clsx';
+import { FileTextIcon, HouseIcon } from 'lucide-react';
 import Link from 'next/link';
 
 export function Menu() {
+  const navClass = clsx(
+    'bg-slate-900 text-slate-100 rounded-lg',
+    'flex flex-col mb-8',
+    'sm:flex-row sm:flex-wrap',
+    'overflow-hidden',
+    // 'h-10',
+  );
+  const linkClass = clsx(
+    '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4',
+    'flex items-center justify-start gap-2',
+    'transition hover:bg-slate-800',
+    'h-10',
+    'shrink-0',
+  );
+
   return (
-    <nav>
-      <a href='/' target='_blank'>
+    <nav className={navClass}>
+      <a className={linkClass} href='/' target='_blank'>
+        <HouseIcon />
         Home
       </a>
 
-      <Link href='/admin/post'>Posts</Link>
+      <Link className={linkClass} href='/admin/post'>
+        <FileTextIcon />
+        Posts
+      </Link>
     </nav>
   );
 }
