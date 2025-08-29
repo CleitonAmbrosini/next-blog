27ec2c30749a3eed6714990fd11d85b0b9fdefbd
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-08-28 23:10:07 -0300

    feat: create open/close navigation menu

--- src/components/Menu/Menu.tsx
+++ src/components/Menu/Menu.tsx
@@ -1,25 +1,60 @@
+'use client';
+
 import clsx from 'clsx';
-import { FileTextIcon, HouseIcon } from 'lucide-react';
+import { CircleXIcon, FileTextIcon, HouseIcon, MenuIcon, PlusIcon } from 'lucide-react';
 import Link from 'next/link';
+import { usePathname } from 'next/navigation';
+import { useEffect, useState } from 'react';
 
 export function Menu() {
+  const [isOpen, setIsOpen] = useState(false);
+  const pathName = usePathname();
+
+  useEffect(() => {
+    setIsOpen(false);
+  }, [pathName]);
+
   const navClass = clsx(
     'bg-slate-900 text-slate-100 rounded-lg',
     'flex flex-col mb-8',
     'sm:flex-row sm:flex-wrap',
-    'overflow-hidden',
-    // 'h-10',
+    !isOpen && 'overflow-hidden',
+    !isOpen && 'h-10',
+    'sm:overflow-visible sm:h-auto',
   );
   const linkClass = clsx(
-    '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4',
+    '[&>svg]:w-[16px] [&>svg]:h-[16px] px-4 cursor-pointer rounded-lg',
     'flex items-center justify-start gap-2',
     'transition hover:bg-slate-800',
     'h-10',
     'shrink-0',
   );
+  const openCloseBtnClass = clsx(
+    linkClass,
+    'text-blue-200 italic',
+    'sm:hidden',
+  );
 
   return (
     <nav className={navClass}>
+      <button
+        onClick={() => setIsOpen(state => !state)}
+        className={openCloseBtnClass}
+      >
+        {!isOpen && (
+          <>
+            <MenuIcon />
+            Open
+          </>
+        )}
+        {isOpen && (
+          <>
+            <CircleXIcon />
+            Close
+          </>
+        )}
+      </button>
+
       <a className={linkClass} href='/' target='_blank'>
         <HouseIcon />
         Home
@@ -28,6 +63,11 @@ export function Menu() {
       <Link className={linkClass} href='/admin/post'>
         <FileTextIcon />
         Posts
+      </Link>
+
+      <Link className={linkClass} href='/admin/post/new'>
+        <PlusIcon />
+        New post
       </Link>
     </nav>
   );
