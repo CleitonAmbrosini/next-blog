c75893415b4bac2e7f36c666548f5f43c308fa28
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-08-30 18:31:11 -0300

    feat: create Button variants

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -4,8 +4,10 @@ export const dynamic = 'force-static';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='py-16 text-6xl'>
-      <Button> Create new </Button>
+    <div className='py-16 text-6xl flex gap-4 flex-wrap'>
+      <Button variant='default' size='sm'> Create new </Button>
+      <Button variant='ghost' size='md'> Create new </Button>
+      <Button variant='danger' size='lg'> Create new </Button>
     </div>
   );
 }
--- src/components/Button/Button.tsx
+++ src/components/Button/Button.tsx
@@ -1,5 +1,31 @@
-type ButtonProps = {} & React.ComponentProps<'button'>;
+import clsx from 'clsx';
 
-export function Button({ children, ...props }: ButtonProps) {
-  return <button {...props}>{children}</button>;
+type ButtonsVariants = 'default' | 'ghost' | 'danger';
+type ButtonsSizes = 'sm' | 'md' | 'lg';
+
+type ButtonProps = {
+  variant: ButtonsVariants;
+  size: ButtonsSizes;
+} & React.ComponentProps<'button'>;
+
+export function Button({ variant, size, children, ...props }: ButtonProps) {
+  const buttonVariants: Record<ButtonsVariants, string> = {
+    default: clsx('bg-blue-600 text-blue-100'),
+    ghost: clsx('bg-slate-200 text-slate-900'),
+    danger: clsx('bg-red-600 text-red-100'),
+  };
+
+  const buttonSizes: Record<ButtonsSizes, string> = {
+    sm: clsx(''),
+    md: clsx(''),
+    lg: clsx(''),
+  };
+
+  const buttonClass = clsx(buttonVariants[variant], buttonSizes[size]);
+
+  return (
+    <button {...props} className={buttonClass}>
+      {children}
+    </button>
+  );
 }
