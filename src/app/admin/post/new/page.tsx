3e01730b7d532f30c3d4734d1be28cfb2d57b5c8
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-09-01 16:07:11 -0300

    feat: create InputText component

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,13 +1,14 @@
-import { Button } from '@/components/Button';
+import { InputText } from '@/components/InputText';
 
 export const dynamic = 'force-static';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='py-16 text-6xl flex gap-4 flex-wrap items-center'>
-      <Button variant='default' size='sm'> Create new </Button>
-      <Button variant='ghost' size='md'> Create new </Button>
-      <Button variant='danger' size='lg'> Create new </Button>
+    <div className='flex flex-col gap-6'>
+      <InputText placeholder='Name' />
+      <div>
+        <InputText placeholder='Surname' />
+      </div>
     </div>
   );
 }
