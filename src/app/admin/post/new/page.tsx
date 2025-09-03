e15fa58dcb926ad20d23ce5186993d0de8b04751
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-09-02 23:20:26 -0300

    feat: create form to create new post

--- src/app/admin/post/new/page.tsx
+++ src/app/admin/post/new/page.tsx
@@ -1,14 +1,23 @@
+import { Button } from '@/components/Button';
 import { InputText } from '@/components/InputText';
 
 export const dynamic = 'force-static';
 
 export default async function AdminPostNewPage() {
   return (
-    <div className='flex flex-col gap-6'>
-      <InputText placeholder='Name' />
-      <div>
-        <InputText placeholder='Surname' />
+    <form action='' className='mb-16'>
+      <div className='flex flex-col gap-6'>
+        <InputText placeholder='Name' />
+        <div>
+          <InputText placeholder='Surname' />
+        </div>
+
+        <div className='mt-4'>
+          <Button type='submit' size='lg' variant='default'>
+            Create
+          </Button>
+        </div>
       </div>
-    </div>
+    </form>
   );
 }
