9266d2bca7c42da3f54c5cfa47f19035189aff2a
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-09-30 21:48:20 -0300

    feat: check if are valid username and password

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,11 +1,43 @@
 'use server';
 
+import { verifyPassword } from '@/lib/login/manage-login';
+
 type LoginActionState = {
   username: string;
   error: string;
 };
 
 export async function loginAction(state: LoginActionState, formData: FormData) {
+  if (!(formData instanceof FormData)) {
+    return {
+      username: '',
+      error: 'Wrong data.',
+    };
+  }
+
+  const username = formData.get('username')?.toString().trim() || '';
+  const password = formData.get('password')?.toString().trim() || '';
+
+  if (!username || !password) {
+    return {
+      username,
+      error: 'Enter username and password',
+    };
+  }
+
+  const isValidUsername = username === process.env.LOGIN_USER;
+  const isValidPassword = await verifyPassword(
+    password,
+    process.env.LOGIN_PASS || '',
+  );
+
+  if (!isValidUsername || !isValidPassword) {
+    return {
+      username,
+      error: 'Invalid username or password',
+    };
+  }
+
   return {
     username: '',
     error: 'teste',
