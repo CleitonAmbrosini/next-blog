6a3f1a44705700fa3df4b74807445ec2f8200371
Cleiton Ambrosini <cleiton.ambro@gmail.com>
2025-09-30 22:12:46 -0300

    feat: create login cookie

--- src/actions/login/login-action.ts
+++ src/actions/login/login-action.ts
@@ -1,6 +1,7 @@
 'use server';
 
-import { verifyPassword } from '@/lib/login/manage-login';
+import { createLoginSession, verifyPassword } from '@/lib/login/manage-login';
+import { redirect } from 'next/navigation';
 
 type LoginActionState = {
   username: string;
@@ -38,8 +39,6 @@ export async function loginAction(state: LoginActionState, formData: FormData) {
     };
   }
 
-  return {
-    username: '',
-    error: 'teste',
-  };
+  await createLoginSession(username);
+  redirect('/admin/post');
 }
